5bbd51f30ed1353c8d0812796f174568f805853e
Blue Ϟ Lightning <jawg45@live.com>
2017-10-12 22:54:49 -0400

    Moved Study quizzer to seperate folder
    Moved quiz to folder and added some static questions-
    questions that do not change not questions on statics :p

---
+++ Chem_Study_Quizzer/Chem_Study_Quizzer.py
@@ -0,0 +1,125 @@
+"""Console program designed to generate and ask random chem eng questions then check your answers for them."""
+from random import sample, randrange, choice, random, shuffle
+from time import sleep
+import Static_Questions
+
+def check_answer(desired, guessed, error_margin):
+    return (desired < guessed + error_margin and desired > guessed - error_margin)
+
+def get_user_input(wanted_type):
+    while True:
+        user_input = input("Please enter your value: ")
+        if user_input == "q" or user_input == "exit":
+            raise SystemExit
+        try:
+            if wanted_type == bool:
+                if user_input.strip() in {"true","t","True","T","yes"}: return True
+                if user_input.strip() in {"false","f","False","F","no"}: return False
+                raise ValueError  # If not true or false
+
+            return wanted_type(user_input)  # Convert input to wanted_type
+        except ValueError:
+            print("Please enter a {}".format(wanted_type))
+
+def temperatures():
+    """Module1 unit conversions with temperatures."""
+    C = randrange(-2742, 4000, 1)/10
+    K = C + 273.15
+    F = C * 1.8 + 32
+    R = F + 459.67
+    all_temperatures = [(C," Celcuis"), (F," Fahrenheit"), (K," Kelvin"), (R," Ranken")]
+    T1,T2 = tuple(sample(set(all_temperatures), 2))
+
+    # Chance of being a interval question instead
+    if choice([True, False]):
+        if T1[1] is " Celcuis" or T1[1] is " Kelvin":
+            T2 = (T1[0] * 1.8, choice([" A Fahrenheit Interval", " A Ranken Interval"]))
+        else:
+            T2 = (T1[0] / 1.8, choice([" A Celcuis Interval", " A Kelvin Interval"]))
+
+    print("\nThis is a temperature question.\n")
+    print("For an temperature of {:0.2f}{}\nPlease find its equivanlent value in{}".format(*T1,T2[1]))
+    guess = get_user_input(float)
+
+    if check_answer(T2[0], guess, 0.2):
+        print("correct")
+    else:
+        print("false the answer is {:0.2f}{}".format(*T2))
+
+def pressure():
+    """Module1 Pressure unit conversions."""
+    atmg = random()
+    mmHgg = atmg * 760
+    Kpag = atmg * 101.325
+    barg = atmg * 1.01325
+    psig = atmg * 14.696
+    all_pressure = [(mmHgg," mmHg gage"),
+                    (atmg," atm gage"),
+                    (Kpag," Kpa gage"),
+                    (psig," psi gage"),
+                    (barg," bar gage"),
+                    (mmHgg - 760," mmHg absolute"),
+                    (atmg - 1," atm absolute"),
+                    (Kpag - 101.325," Kpa absolute"),
+                    (psig - 14.696," psi absolute"),
+                    (barg - 1.01323," bar absolute")]
+    P1,P2 = tuple(sample(set(all_pressure), 2))
+
+    print("\nThis is a Pressure question.\n")
+    print("For an Pressure of {:0.2f}{}\nPlease find its equivanlent value in{}".format(*P1,P2[1]))
+    guess = get_user_input(float)
+
+    if check_answer(P2[0], guess, 0.2):
+        print("correct")
+    else:
+        # TODO FIX decimal places?
+        print("false the answer is {:0.2f}{}".format(*P2))
+
+
+A = {"molar_mass":44.01, "name":"A"}
+B = {"molar_mass":12.02, "name":"B"}
+C = {"molar_mass":16.00, "name":"C"}
+D = {"molar_mass":23.5, "name":"D"}
+def fraction():
+    """Module1 Mass and molar fractions."""
+    substances = [A, B, C, D]
+    units = {"lb mole":1, "lb":1, "kg":0.45359237, "kg mole":1}
+
+    substance = choice(substances)
+    print("\nThis is a mass and molar fractions question.\n")
+    print("For a substance '{}' with a molar mass of '{}'\n".format(substance("name"), substance("molar_mass")))
+    print("What is the ")
+    pass
+
+def flow_rate():
+    """Module1 flow_rate."""
+    print("\nThis is a flow rate question.\n")
+    pass
+
+def premade():
+    """List of premade questions- if you get one right its removed from potential questions."""
+    global all_questions
+
+    choice(all_questions)
+    temp = all_questions.pop()
+    question,answer = temp
+    print("\n\n{}\n".format(question))
+    guess = get_user_input(type(answer))
+    if guess is answer:
+        print("Correct the answer is: {}".format(answer))
+    else:
+        print("Incorrect the answer is: {}, This question will be asked again.".format(answer))
+        all_questions.append(temp)  # Add it back
+
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("This is the B-Tech Chem Eng study program.")
+print("It should ask you as many random unit test questions to help you study for the first midterm")
+print("At any time you can type 'q' or 'exit' to exit the program")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+# Main loop
+all_questions = Static_Questions.get_all_questions()
+shuffle(all_questions)
+while True:
+    choice([temperatures(), pressure(), premade()])
+    sleep(5000)
---
+++ Chem_Study_Quizzer/Static_Questions.py
@@ -0,0 +1,33 @@
+"""Storage module to contain all the unchanging questions."""
+import random
+
+# List of true or questions along with thire answer.
+true_false_questions = [
+ ("Is [Input Rate + Generation Rate == Output Rate + Consumption Rate + Accumulation Rate] a Integral Ballance Question? T/F", False),
+ ("An Integral Ballance Has units of the Ballanced quatity (Amount of input vs rate)? T/F", True),
+ ("If there are zero degrees of freedom the question can easily be solved? T/F", True),
+ ("Can a question with 2 degree of freedom be solved right away? T/F", False),
+ ("Are more degrees of freedom good? They make you more free so you can do more stuff? T/F", False),
+ ("Is this correct? An Integral Ballance Is typically applied to continuous processes while a Differential is applied to Batch processes? T/F", False)
+]
+
+# List of multiple choice questions along with thire answer.
+multiple_choice_questions = [
+ ("""Which of the following are incorrect for a Differential Ballance Question.
+ [A]♦ All units are divided per time.
+ [B]♦ Provides what happens at every instance in time.
+ [C]♦ Proves what happens Between two instances in time.
+ [D]♦ Typically applied to continuous processes.
+ ""","C"),
+ ("""Which of the following are incorrect?
+ [A]♦ If a substance is total mass Generation is = Accumulation.
+ [B]♦ If a system is in steady state Accumulation = 0
+ [C]♦ If a substance is nonreactive Generation = Consumption = 0
+ [D]♦ If a system is in steady state total mass is Input = Output.
+ ""","A")
+]
+
+def get_all_questions():
+    """Return all questions stored in this file."""
+    print("Loaded Static_Questions correctly")
+    return multiple_choice_questions + true_false_questions
